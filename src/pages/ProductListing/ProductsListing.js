c94466b8813b0f09989ae1cd64a19597ed9eb0c3
kavyaMVG <kavya.mvg@gmail.com>
2025-06-11 10:45:33 +0530

    Error handling in product listing

--- src/pages/ProductListing/ProductsListing.js
+++ src/pages/ProductListing/ProductsListing.js
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import ProductCard from "./../../components/ProductCard/ProductCard";
 import "./productsListing.css";
 import { CircularProgress } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const ProductsListing = () => {
   const [products, setProducts] = useState([]);
   const [initialProducts, setInitialProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     setLoading(true);
